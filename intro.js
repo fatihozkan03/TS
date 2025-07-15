1716e359020e250640d7f9f2529909f4ea3a0a8c
Fatih Özkan <fatihozkan03@hotmail.com>
2025-07-15 22:03:34 +0300

    first commit

---
+++ intro.js
@@ -0,0 +1,17 @@
+function greeter(name) {
+    return "merhaba" + name;
+}
+var mesaj = greeter(' Ali');
+console.log(mesaj);
+var sayi;
+sayi = 10;
+sayi = 10.4;
+var sehir;
+sehir = "İstanbul";
+var dogruMu = false;
+dogruMu = true;
+var sayilar = [123, 124, 125];
+var sayilar2 = [123, 124, 125];
+var degerler = [123, "Ali", 125, "Veli", 2, "Ayşe", "Fatma", "Zeynep", "Mehmet"];
+var değerler2 = [123, "Ali", true,];
+console.log(degerler[2].toString());
---
+++ intro.ts
@@ -0,0 +1,44 @@
+function greeter(name: string) {
+    return "merhaba" + name;
+}
+
+let mesaj = greeter(' Ali');
+
+console.log(mesaj);
+
+let sayi: number
+
+sayi = 10;
+sayi = 10.4;
+
+let sehir: string;
+sehir = "İstanbul";
+
+let dogruMu: boolean = false;
+dogruMu = true;
+
+let sayilar: number[] = [123,124,125];
+
+let sayilar2 : Array<number> = [123,124,125];
+
+let degerler: (number | string)[] = [123, "Ali", 125, "Veli",2, "Ayşe", "Fatma", "Zeynep", "Mehmet"];
+
+let değerler2: [number, string, boolean] = [123, "Ali", true,];
+
+console.log(degerler[2].toString());
+
+enum Renk {
+    Kirmizi = 1,
+    Yeşil = 2,
+    Mavi = 3}
+
+let renk: Renk = Renk.Kirmizi;
+
+let deger : any = 123;
+deger = "ali";
+
+let deger2 : void = undefined;
+
+function selamVer2() : void {
+    console.log("Selam");
+}
