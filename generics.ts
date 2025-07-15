fc2c658cf940b334409091849c1cf01203c8a9dc
Fatih Özkan <fatihozkan03@hotmail.com>
2025-07-16 01:33:37 +0300

    Add TypeScript implementations for generics and loops

---
+++ generics.ts
@@ -0,0 +1,21 @@
+function deger(x: number): number {
+    return x;
+}
+
+function deger2(x: string): string {
+    return x;
+}
+
+let sayi = deger(10)
+console.log(sayi)
+
+let sehir = deger2("ankara")
+console.log(sehir)
+
+function deger3<T>(x:T):T{
+    console.log(x)
+    return x;
+}
+
+let sayi3 = deger3<number>(2)
+let sehir3 = deger3<string>("izmir")
